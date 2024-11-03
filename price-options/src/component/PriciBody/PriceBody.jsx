d5287dd2df505afd218518030a7eb0daaab861af
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-03 16:57:30 +0600

    head tage add

--- price-options/src/component/PriciBody/PriceBody.jsx
+++ price-options/src/component/PriciBody/PriceBody.jsx
@@ -1,6 +1,8 @@
 const PriceBody = () => {
     return (
         <div>
+            <h2>About price details in price body.</h2>
+            <h3>Perfectly product price has in this page.</h3>
             <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed unde voluptate consequatur quos assumenda mollitia, ipsam, aspernatur est doloremque molestiae, id sunt! Quasi quisquam nobis odio. Amet obcaecati inventore repellat?
             </p>
