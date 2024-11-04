106a27c9e8fcf41c6cf8b3e1cd923e5865a98fde
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-04 16:52:36 +0600

    some pera add

--- price-options/src/component/PriciBody/PriceBody.jsx
+++ price-options/src/component/PriciBody/PriceBody.jsx
@@ -6,6 +6,12 @@ const PriceBody = () => {
             <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed unde voluptate consequatur quos assumenda mollitia, ipsam, aspernatur est doloremque molestiae, id sunt! Quasi quisquam nobis odio. Amet obcaecati inventore repellat?
             </p>
+            <hr />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat laboriosam veniam accusantium perspiciatis totam, quis sed voluptates architecto vitae voluptatibus consequatur unde a expedita. Neque officiis quisquam itaque cum? Magnam, ipsa. Earum ad accusamus excepturi commodi omnis, repellendus maxime doloribus mollitia sint! Quisquam sequi sint architecto vitae, cum non hic.
+            </p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione expedita perspiciatis assumenda numquam, exercitationem, similique aliquid vitae dolorem quod possimus quas debitis sequi blanditiis commodi veniam excepturi, odio minus corporis?
+
+            </p>
         </div>
     );
 };
