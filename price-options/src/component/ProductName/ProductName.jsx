635d76e17f95f061b17c9ce1d1bd01ced515a21f
Alal Hosen <alalhosen1992620120@gmail.com>
2024-11-05 20:52:12 +0600

    unorder list

---
+++ price-options/src/component/ProductName/ProductName.jsx
@@ -0,0 +1,23 @@
+const ProductName = () => {
+    return (
+        <div>
+            <h2>Product Name</h2>
+            <ul>
+                <li>Computer</li>
+                <li>Mouse</li>
+                <li>Keybord</li>
+                <li>PC</li>
+                <li>Leptop</li>
+            </ul>
+            <ul>
+                <li>Apple</li>
+                <li>Orange</li>
+                <li>Date</li>
+                <li>Lemon</li>
+                <li>Water Milon`</li>
+            </ul>
+        </div>
+    );
+};
+
+export default ProductName;
